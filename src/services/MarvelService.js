cc4c18b7a2fbb2afb7048990cf05cd60820b1aac
yura4ch <yura4ch@gmail.com>
2022-09-27 15:26:14 +0300

    added new component char info

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -26,11 +26,13 @@ class  MarvelService {
 
     _transformCharacter = (char) => { 
         return {
+            id: char.id,
             name:char.name,
-            description: char.description.length > 220 ? (`${char.description.slice(0,220)}...`) : char.description,
+            description: char.description.length > 120 ? (`${char.description.slice(0,120)}...`) : 'Опис відсутній :(',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items
         }
     }
 }
